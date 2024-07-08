da6df94023276c4917e2d02e46c3e55723f8b403
computerpsycho <ghcomputerpsycho@riseup.net>
2024-07-08 15:03:41 +0100

    feat: add fish

--- configure.sh
+++ configure.sh
@@ -2,5 +2,7 @@
 CONFIG_PATH="$HOME/.config/"
 
 echo "Updating dotfiles"
+
 install helix/config.toml "$CONFIG_PATH/helix/config.toml" 
 install i3/config "$CONFIG_PATH/i3/config"
+install fish/config.fish "$CONFIG_PATH/fish/config.fish"
---
+++ fish/config.fish
@@ -0,0 +1,14 @@
+set -gx PATH $PATH ~/.cargo/bin
+set -gx HELIX_RUNTIME ~/Sources/helix/runtime
+set -gx EDITOR helix
+set -gx TERMINAL kitty
+# Created by `pipx` on 2024-07-07 07:27:43
+set PATH $PATH /home/vuax/.local/bin
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx PATH "$VOLTA_HOME/bin" $PATH
+
+set fish_greeting
+set -U fish_prompt_pwd_dir_length 0 # show full path
+
+alias hx helix # arch issue (conflicting packages)
+alias lg lazygit
