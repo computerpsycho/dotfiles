696edfe8b2f3f14c59669c5a7189641eccfc8581
computerpsycho <ghcomputerpsycho@riseup.net>
2024-07-09 08:54:15 +0100

    chore: minor

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,4 @@
 set -gx PATH $PATH ~/.cargo/bin
-set -gx HELIX_RUNTIME ~/Sources/helix/runtime
 set -gx EDITOR helix
 set -gx TERMINAL kitty
 # Created by `pipx` on 2024-07-07 07:27:43
@@ -10,5 +9,6 @@ set -gx PATH "$VOLTA_HOME/bin" $PATH
 set fish_greeting
 set -U fish_prompt_pwd_dir_length 0 # show full path
 
+alias v nvim
 alias hx helix # arch issue (conflicting packages)
 alias lg lazygit
