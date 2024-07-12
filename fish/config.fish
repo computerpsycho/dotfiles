e140783fc655558dc4c06a54e15257f433297631
computerpsycho <ghcomputerpsycho@riseup.net>
2024-07-12 19:24:17 +0100

    feat(fish): change $EDITOR

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
 set -gx PATH $PATH ~/.cargo/bin
 set -gx PATH $PATH ~/go/bin/
-set -gx EDITOR helix
+set -gx EDITOR nvim
 set -gx TERMINAL kitty
 # Created by `pipx` on 2024-07-07 07:27:43
 set PATH $PATH /home/vuax/.local/bin
